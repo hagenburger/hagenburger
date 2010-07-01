d6aea67d4cb615bf0df6c420c3e26fb4f3c9f6a0
Nico Hagenburger <nico@hagenburger.net>
2010-07-01 11:10:34 +0200

    new blog post: lemonade css sprites for sass and compass

--- config/compass.rb
+++ config/compass.rb
@@ -2,6 +2,7 @@ ROOT = File.join(File.dirname(__FILE__), '/')
 puts "Site root is: " + File.expand_path(ROOT)
 
 require "redgreengrid" # version > 0.3.2
+require "lemonade"
  
 output_style = :compressed
 project_path = ROOT               # must be set for Compass to work 
@@ -9,4 +10,4 @@ sass_dir     = "stylesheets"  # dir containing Sass / Compass source files
 http_path    = "/"                # root when deployed
 css_dir      = "../site/stylesheets" # final CSS
 images_dir   = "../site/images"      # final images
-http_images_path = "../images"
+http_images_path = "/images"
