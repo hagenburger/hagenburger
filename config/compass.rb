5791ca8855ff071fb3275c2a9516dc58d6c1c0f9
Nico Hagenburger <nico@hagenburger.net>
2010-10-29 09:08:57 +0200

    new blog post: production ready staticmatic projects

--- config/compass.rb
+++ config/compass.rb
@@ -2,11 +2,11 @@ ROOT = File.join(File.dirname(__FILE__), '/')
 puts "Site root is: " + File.expand_path(ROOT)
 
 require "redgreengrid" # version > 0.3.2
- 
-output_style = :compressed
-project_path = ROOT               # must be set for Compass to work 
-sass_dir     = "../src/stylesheets"  # dir containing Sass / Compass source files
-http_path    = "/"                # root when deployed
-css_dir      = "../site/stylesheets" # final CSS
-images_dir   = "../site/images"      # final images
-http_images_path = "/images"
+
+output_style     = :compressed
+project_path     = ROOT
+sass_dir         = "../src/stylesheets"
+http_path        = "../"
+css_dir          = "../site/stylesheets"
+images_dir       = "../site/images"
+http_images_path = "../images"
